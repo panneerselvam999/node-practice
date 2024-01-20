e9291d7ae9dab422029300eeb01526788285c31c
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-01-20 15:18:05 -0500

    print form input value in console

--- NodeServer.js
+++ NodeServer.js
@@ -172,11 +172,144 @@
 // server.listen(PORT);
 // console.log("Server is running on : " + PORT);
 
+// // _________________________________________________________________________________________________
+
+// const http = require("http");
+// const fs = require("fs");
+// const { text } = require("stream/consumers");
+// const PORT = 3500;
+
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   const method = req.method;
+//   if (url === "/") {
+//     res.setHeader("Content-type", "text/html");
+//     res.write("<html>");
+//     res.write("<head> <title> Login </title> </head>");
+//     // res.write(
+//     //   '<body> <h1> Login Form</h1> <form enctype="multipart/form-data" action="/message" method="POST"> <input type="text" name="message"> <input type="submit" value="send"> </form> </body>'
+//     // );
+//     res.write(
+//       "<html>" +
+//         "<head>" +
+//         "<title>Login</title>" +
+//         "</head>" +
+//         "<body>" +
+//         "<h1>Login Form</h1>" +
+//         '<form action="/message" method="POST" enctype="multipart/form-data">' +
+//         '<input type="text" name="message">' +
+//         '<input type="file" name="file">' +
+//         '<input type="submit" value="send">' +
+//         "</form>" +
+//         "<p>panneer selvam</p>" + // Text moved within the body
+//         "</body>" +
+//         "</html>"
+//     );
+//     res.write("</html>");
+//     res.write("panneer selvam");
+//     return res.end();
+//   }
+
+//   if (url === "/message" && method == "POST") {
+//     req.on("data", (chunk) => {
+//       console.log("Chunk data : " + chunk);
+//       console.log("chunk data : ");
+//       console.log(chunk);
+//     });
+
+//     fs.writeFileSync("first.txt", "create text file");
+//     res.setHeader("Location", "/");
+//     res.statusCode = 302;
+//     return res.end();
+//   }
+
+//   console.log(req.url);
+//   res.setHeader("Content-type", "text/html");
+//   res.write("<html>");
+//   res.write("<head> <title> Node JS </title> </head>");
+//   res.write("<body> <h1> Hello World </h1> </body>");
+//   res.write("</html>");
+//   res.write("panneer selvam");
+//   res.end();
+// });
+// server.listen(PORT);
+// console.log("Server is running on : " + PORT);
+// _________________________________________________________________________________________________
+
+// const http = require("http");
+// const fs = require("fs");
+// // const { text } = require("stream/consumers");
+// const PORT = 3500;
+
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   const method = req.method;
+//   if (url === "/") {
+//     res.setHeader("Content-type", "text/html");
+//     res.write("<html>");
+//     res.write("<head> <title> Login </title> </head>");
+//     res.write(
+//       '<body> <h1> Login Form</h1> <form enctype="multipart/form-data" action="/message" method="POST"> <input type="text" name="message"> <input type="submit" value="send"> </form> </body>'
+//     );
+
+//     res.write("</html>");
+//     res.write("panneer selvam");
+//     return res.end();
+//   }
+
+//   if (url === "/message" && method == "POST") {
+//     const body = [];
+//     req.on("data", (chunk) => {
+//       body.push(chunk);
+//       console.log("Chunk data : ");
+//       console.log(chunk);
+//     });
+
+//     req.on("end", () => {
+//       const parsedBody = Buffer.concat(body).toString();
+//       console.log("parsedBody : ");
+//       console.log(parsedBody);
+//     });
+//     // req.on("end", () => {
+//     //   const parsedBody = Buffer.concat(body).toString(); // its a chart GPT code
+
+//     //   // Find the Content-Disposition header
+//     //   const contentDispositionIndex = parsedBody.indexOf("Content-Disposition");
+//     //   if (contentDispositionIndex !== -1) {
+//     //     const contentDisposition = parsedBody.substring(
+//     //       contentDispositionIndex,
+//     //       parsedBody.indexOf("\n")
+//     //     );
+
+//     //     console.log("Content-Disposition: ");
+//     //     console.log(contentDisposition);
+//     //   } else {
+//     //     console.log("Content-Disposition not found in the parsed body.");
+//     //   }
+//     // });
+
+//     fs.writeFileSync("first.txt", "create text file");
+//     res.setHeader("Location", "/");
+//     res.statusCode = 302;
+//     return res.end();
+//   }
+
+//   console.log(req.url);
+//   res.setHeader("Content-type", "text/html");
+//   res.write("<html>");
+//   res.write("<head> <title> Node JS </title> </head>");
+//   res.write("<body> <h1> Hello World </h1> </body>");
+//   res.write("</html>");
+//   res.write("panneer selvam");
+//   res.end();
+// });
+// server.listen(PORT);
+// console.log("Server is running on : " + PORT);
 // _________________________________________________________________________________________________
 
 const http = require("http");
 const fs = require("fs");
-const { text } = require("stream/consumers");
+// const { text } = require("stream/consumers");
 const PORT = 3500;
 
 const server = http.createServer((req, res) => {
@@ -186,36 +319,45 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-type", "text/html");
     res.write("<html>");
     res.write("<head> <title> Login </title> </head>");
-    // res.write(
-    //   '<body> <h1> Login Form</h1> <form enctype="multipart/form-data" action="/message" method="POST"> <input type="text" name="message"> <input type="submit" value="send"> </form> </body>'
-    // );
     res.write(
-      "<html>" +
-        "<head>" +
-        "<title>Login</title>" +
-        "</head>" +
-        "<body>" +
-        "<h1>Login Form</h1>" +
-        '<form action="/message" method="POST" enctype="multipart/form-data">' +
-        '<input type="text" name="message">' +
-        '<input type="file" name="file">' +
-        '<input type="submit" value="send">' +
-        "</form>" +
-        "<p>panneer selvam</p>" + // Text moved within the body
-        "</body>" +
-        "</html>"
+      '<body> <h1> Login Form</h1> <form enctype="multipart/form-data" action="/message" method="POST"> <input type="text" name="message"> <input type="submit" value="send"> </form> </body>'
     );
+
     res.write("</html>");
     res.write("panneer selvam");
     return res.end();
   }
 
   if (url === "/message" && method == "POST") {
+    const body = [];
     req.on("data", (chunk) => {
-      console.log("Chunk data : " + chunk);
-      console.log("chunk data : ");
+      body.push(chunk);
+      console.log("Chunk data : ");
       console.log(chunk);
     });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log("parsedBody : ");
+      console.log(parsedBody);
+    });
+    // req.on("end", () => {
+    //   const parsedBody = Buffer.concat(body).toString(); // its a chart GPT code
+
+    //   // Find the Content-Disposition header
+    //   const contentDispositionIndex = parsedBody.indexOf("Content-Disposition");
+    //   if (contentDispositionIndex !== -1) {
+    //     const contentDisposition = parsedBody.substring(
+    //       contentDispositionIndex,
+    //       parsedBody.indexOf("\n")
+    //     );
+
+    //     console.log("Content-Disposition: ");
+    //     console.log(contentDisposition);
+    //   } else {
+    //     console.log("Content-Disposition not found in the parsed body.");
+    //   }
+    // });
 
     fs.writeFileSync("first.txt", "create text file");
     res.setHeader("Location", "/");
