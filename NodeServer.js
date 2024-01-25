40dea456d1eb8611fdef1d7b2246b2caa34d3790
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-01-25 05:52:45 -0500

    routes file

--- NodeServer.js
+++ NodeServer.js
@@ -305,74 +305,125 @@
 // });
 // server.listen(PORT);
 // console.log("Server is running on : " + PORT);
+// // _________________________________________________________________________________________________
+
+// const http = require("http");
+// const fs = require("fs");
+// // const { text } = require("stream/consumers");
+// const PORT = 3500;
+
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   const method = req.method;
+//   if (url === "/") {
+//     res.setHeader("Content-type", "text/html");
+//     res.write("<html>");
+//     res.write("<head> <title> Login </title> </head>");
+//     res.write(
+//       '<body> <h1> Login Form</h1> <form enctype="multipart/form-data" action="/message" method="POST"> <input type="text" name="message"> <input type="submit" value="send"> </form> </body>'
+//     );
+
+//     res.write("</html>");
+//     res.write("panneer selvam");
+//     return res.end();
+//   }
+
+//   if (url === "/message" && method == "POST") {
+//     const body = [];
+//     req.on("data", (chunk) => {
+//       body.push(chunk);
+//       console.log("Chunk data : ");
+//       console.log(chunk);
+//     });
+
+//     req.on("end", () => {
+//       const parsedBody = Buffer.concat(body).toString();
+//       const message = parsedBody.split("=");
+//       fs.writeFileSync("hello.txt", message[1]);
+//       console.log("message : ");
+//       console.log(message);
+//       console.log("parsedBody : ");
+//       console.log(parsedBody);
+//     });
+
+//     fs.writeFileSync("first.txt", "create text file");
+//     res.setHeader("Location", "/");
+//     res.statusCode = 302;
+//     return res.end();
+//   }
+
+//   console.log(req.url);
+//   res.setHeader("Content-type", "text/html");
+//   res.write("<html>");
+//   res.write("<head> <title> Node JS </title> </head>");
+//   res.write("<body> <h1> Hello World </h1> </body>");
+//   res.write("</html>");
+//   res.write("panneer selvam");
+//   res.end();
+// });
+// server.listen(PORT);
+// console.log("Server is running on : " + PORT);
+// // _________________________________________________________________________________________________
+
+// const http = require("http");
+// const fs = require("fs");
+// const { text } = require("stream/consumers");
+// const PORT = 3500;
+
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   const method = req.method;
+//   if (url === "/") {
+//     res.setHeader("Content-type", "text/html");
+//     res.write("<html>");
+//     res.write("<head> <title> Login </title> </head>");
+//     res.write(
+//       '<body> <h1> Login Form</h1> <form enctype="multipart/form-data" action="/message" method="POST"> <input type="text" name="message"> <input type="submit" value="send"> </form> </body>'
+//     );
+
+//     res.write("</html>");
+//     res.write("panneer selvam");
+//     return res.end();
+//   }
+
+//   if (url === "/message" && method == "POST") {
+//     const body = [];
+//     req.on("data", (chunk) => {
+//       body.push(chunk);
+//     });
+
+//     req.on("end", () => {
+//       const parsedBody = Buffer.concat(body).toString();
+//       const message = parsedBody.split("=");
+//       fs.writeFileSync("hello.txt", "create text file");
+//       fs.writeFileSync("hello.txt", message[1]);
+//     });
+
+//     res.setHeader("Location", "/");
+//     res.statusCode = 302;
+//     return res.end();
+//   }
+
+//   console.log(req.url);
+//   res.setHeader("Content-type", "text/html");
+//   res.write("<html>");
+//   res.write("<head> <title> Node JS </title> </head>");
+//   res.write("<body> <h1> Hello World </h1> </body>");
+//   res.write("</html>");
+//   res.write("panneer selvam");
+//   res.end();
+// });
+// server.listen(PORT);
+// console.log("Server is running on : " + PORT);
 // _________________________________________________________________________________________________
 
 const http = require("http");
 const fs = require("fs");
 // const { text } = require("stream/consumers");
+const routes = require("./routes");
 const PORT = 3500;
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.setHeader("Content-type", "text/html");
-    res.write("<html>");
-    res.write("<head> <title> Login </title> </head>");
-    res.write(
-      '<body> <h1> Login Form</h1> <form enctype="multipart/form-data" action="/message" method="POST"> <input type="text" name="message"> <input type="submit" value="send"> </form> </body>'
-    );
-
-    res.write("</html>");
-    res.write("panneer selvam");
-    return res.end();
-  }
-
-  if (url === "/message" && method == "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-      console.log("Chunk data : ");
-      console.log(chunk);
-    });
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log("parsedBody : ");
-      console.log(parsedBody);
-    });
-    // req.on("end", () => {
-    //   const parsedBody = Buffer.concat(body).toString(); // its a chart GPT code
-
-    //   // Find the Content-Disposition header
-    //   const contentDispositionIndex = parsedBody.indexOf("Content-Disposition");
-    //   if (contentDispositionIndex !== -1) {
-    //     const contentDisposition = parsedBody.substring(
-    //       contentDispositionIndex,
-    //       parsedBody.indexOf("\n")
-    //     );
-
-    //     console.log("Content-Disposition: ");
-    //     console.log(contentDisposition);
-    //   } else {
-    //     console.log("Content-Disposition not found in the parsed body.");
-    //   }
-    // });
-
-    fs.writeFileSync("first.txt", "create text file");
-    res.setHeader("Location", "/");
-    res.statusCode = 302;
-    return res.end();
-  }
-
-  console.log(req.url);
-  res.setHeader("Content-type", "text/html");
-  res.write("<html>");
-  res.write("<head> <title> Node JS </title> </head>");
-  res.write("<body> <h1> Hello World </h1> </body>");
-  res.write("</html>");
-  res.write("panneer selvam");
-  res.end();
-});
+// const server = http.createServer((req, res) => {});
+const server = http.createServer(routes);
 server.listen(PORT);
 console.log("Server is running on : " + PORT);
